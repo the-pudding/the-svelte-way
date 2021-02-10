c41854a2fbc96da3005b7c3b30e5f6c894e64b85
Amelia <wattenberger@gmail.com>
2021-02-10 11:58:15 -0500

    more comments!

--- src/components/episodes/e2/Root.svelte
+++ src/components/episodes/e2/Root.svelte
@@ -15,11 +15,17 @@ Agenda:
 - the basics of tweened: InterpolatedNumber
   - look! we can change the duration
   - look! we can change the delay
+  - look! we can change the easing
 - a more visual example: Dots
   - look! we can use an array
   - look! we can use an object (optional)
 - switch to spring
   - what are stiffness & damping?
+  - pros/cons
+    - more "lifelike"
+  - cons
+    - can't set duration/delay/easing
+      - useful for syncing with eg other animations
 - look! we can make custom transitions (optional)
 - fun examples?
   - polygraph site background dots
